a0a6bafd967660980c5823913069f1601459d445
kim <smkim000721@naver.com>
2025-05-23 16:46:09 +0900

    커밋

---
+++ src/main/java/com/example/signapp2/Signapp2Application.java
@@ -0,0 +1,13 @@
+package com.example.signapp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Signapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Signapp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/signapp2/Signapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.signapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Signapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
